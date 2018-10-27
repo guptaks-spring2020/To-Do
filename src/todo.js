8913e3c6116c23587f2b829218e66a765229e110
Jay Chitalia <chitalia.j@husky.neu.edu>
2018-10-27 00:29:46 -0400

    Hosted Json file locally and created proper folder structure, minor changes.

--- src/todo.js
+++ src/todo.js
@@ -1,13 +1,14 @@
-window.onload = function(){
+window.onload = function () {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.myjson.com/bins/13lpkc');
+    jsondata = "./assets/todo.json"
+    xhr.open('GET', jsondata, true);
     xhr.responseType = 'text';
 
     xhr.onload = function () {
-        if(this.status == 200){
+        if (this.status == 200) {
             let data = JSON.parse(xhr.responseText);
-            loadData(data);    
-        }    
+            loadData(data);
+        }
     };
     xhr.send();
 };
@@ -16,21 +17,21 @@ window.onload = function(){
 function loadData(data) {
     //alert(JSON.stringify(data));
 
-    
+
     for (let i = 0; i < data.length; i++) {
-       
+
 
         let TodoTitle = document.createTextNode(data[i].Title);
         let TodoAuthor = document.createTextNode(data[i].Author);
         let TodoDate = document.createTextNode(data[i].date);
-       // let todoTextItem = document.createTextNode(data[i].ToDoText);
+        // let todoTextItem = document.createTextNode(data[i].ToDoText);
         let todoTextItem = data[i].ToDoText;
         // for(let j = 0; j< data[i].ToDoText.length; j++){
         //     todoTextItem = document.createTextNode(data[i].ToDoText[j]);
         //     console.log(todoTextItem);
         // }
-        addJson(TodoTitle, TodoAuthor,TodoDate, todoTextItem);
-    } 
+        addJson(TodoTitle, TodoAuthor, TodoDate, todoTextItem);
+    }
 }
 
 
@@ -43,26 +44,26 @@ function addJson(TodoTitle, TodoAuthor, TodoDate, todoTextItem) {
     spanParent.setAttribute("class", "spanParent");
     span.setAttribute("class", "containers");
     let textdiv = document.createElement("div");
-     let dateAuthor = document.createElement("div");
+    let dateAuthor = document.createElement("div");
     //let date = new Date().toLocaleDateString();
-     let dateText = document.createTextNode(TodoDate.data);
-     let authorText = document.createTextNode(TodoAuthor.data);
-     dateAuthor.appendChild(dateText);
-    
-     let authorDiv = document.createElement("div");
-     authorDiv.appendChild(authorText)
-     authorDiv.setAttribute("class","authorDiv");
+    let dateText = document.createTextNode(TodoDate.data);
+    let authorText = document.createTextNode(TodoAuthor.data);
+    dateAuthor.appendChild(dateText);
 
-     dateAuthor.appendChild(authorDiv);
-     dateAuthor.setAttribute("class", "dateauthor");
+    let authorDiv = document.createElement("div");
+    authorDiv.appendChild(authorText)
+    authorDiv.setAttribute("class", "authorDiv");
+
+    dateAuthor.appendChild(authorDiv);
+    dateAuthor.setAttribute("class", "dateauthor");
     let title = document.getElementById("titlex").value;
     let titleText;
-    
+
     titleText = document.createTextNode(TodoTitle.data);
     textdiv.appendChild(titleText);
-    textdiv.setAttribute("class", "textDiv")  
-    
-    
+    textdiv.setAttribute("class", "textDiv")
+
+
     document.createElement("br");
     let todoItem = document.createElement("input");
     todoItem.setAttribute("class", "taskInput");
@@ -82,14 +83,14 @@ function addJson(TodoTitle, TodoAuthor, TodoDate, todoTextItem) {
 
     let ul = document.createElement("ul");
     ul.setAttribute("class", "uList");
-    
-    for(let i = 0; i< todoTextItem.length; i++){
+
+    for (let i = 0; i < todoTextItem.length; i++) {
         let li = document.createElement("li");
-    li.setAttribute("class", "list");
+        li.setAttribute("class", "list");
         let create = document.createTextNode(todoTextItem[i])
         li.appendChild(create);
-        ul.appendChild(li);  
-        spanParent.appendChild(ul); 
+        ul.appendChild(li);
+        spanParent.appendChild(ul);
         span.appendChild(spanParent);
         let cl = close();
         li.appendChild(cl);
@@ -97,20 +98,20 @@ function addJson(TodoTitle, TodoAuthor, TodoDate, todoTextItem) {
         li.addEventListener("click", strikeout);
     }
 
-    
+
 
     iconAdd.addEventListener("click", function () {
         let todoItemValue = todoItem.value;
         let todoItemText1 = document.createTextNode(todoItemValue);
         let li = document.createElement("li");
         li.setAttribute("class", "list");
-        
+
         // let ul = document.createElement("ul");
         // ul.setAttribute("class", "uList");
         let cl = close();
 
         if (todoItemValue === '') {
-            //alert("Please enter some value");
+            alert("Please enter some value");
         } else {
             li.appendChild(todoItemText1);
             ul.appendChild(li);
@@ -126,8 +127,7 @@ function addJson(TodoTitle, TodoAuthor, TodoDate, todoTextItem) {
 
 
 
-function close()
-{
+function close() {
     let close = document.createElement("i");
     close.setAttribute("class", "material-icons")
     close.innerHTML = 'delete';
@@ -139,113 +139,109 @@ let btn = document.getElementById("btn");
 btn.addEventListener("click", addToDoList);
 
 
-function addToDoList(){
+function addToDoList() {
     let div = document.getElementById("container1");
     let span = document.createElement("div");
     let spanParent = document.createElement("div");
-        spanParent.setAttribute("class", "spanParent");
+    spanParent.setAttribute("class", "spanParent");
     span.setAttribute("class", "containers");
     let textdiv = document.createElement("div");
 
 
-     let dateAuthor = document.createElement("div");
-     let date = new Date().toLocaleDateString();
-     let dateText = document.createTextNode(date);
+    let dateAuthor = document.createElement("div");
+    let date = new Date().toLocaleDateString();
+    let dateText = document.createTextNode(date);
 
-     let author = document.getElementById("authoruser").innerHTML;
+    let author = document.getElementById("authoruser").innerHTML;
     let authortext = document.createTextNode(author);
-    
-     let authorDiv = document.createElement("div");
-     authorDiv.appendChild(authortext);
-     authorDiv.setAttribute("class","authorDiv");
 
-     
-
-     dateAuthor.appendChild(dateText);
-     dateAuthor.appendChild(authorDiv);
-     dateAuthor.setAttribute("class", "dateauthor");
+    let authorDiv = document.createElement("div");
+    authorDiv.appendChild(authortext);
+    authorDiv.setAttribute("class", "authorDiv");
 
 
-     
+
+    dateAuthor.appendChild(dateText);
+    dateAuthor.appendChild(authorDiv);
+    dateAuthor.setAttribute("class", "dateauthor");
+
+
+
     let title = document.getElementById("titlex").value;
     let titleText = document.createTextNode(title);
     let ul = document.createElement("ul");
     ul.setAttribute("class", "uList");
     let iconAdd = document.createElement("i");
-    iconAdd.setAttribute("class","fa fa-plus");
+    iconAdd.setAttribute("class", "fa fa-plus");
     let todoItem = document.createElement("input");
-    todoItem.setAttribute("class","taskInput");
+    todoItem.setAttribute("class", "taskInput");
     todoItem.type = 'text';
     let iconDel = document.createElement("i");
     iconDel.innerHTML = "delete";
-    iconDel.setAttribute("class","material-icons");
+    iconDel.setAttribute("class", "material-icons");
     iconDel.addEventListener("click", deleteToDoList);
-    if(title==="")
-    {
+    if (title === "") {
 
         alert("Please enter a title");
-    }
+    } else {
+        textdiv.appendChild(titleText);
 
-    else{
-    textdiv.appendChild(titleText);
-    
-    textdiv.setAttribute("class", "textDiv")
-    document.createElement("br");
-    // let todoItem = document.createElement("input");
-    // todoItem.setAttribute("class","taskInput");
-    // todoItem.type = 'text';
-    // let iconAdd = document.createElement("i");
-    // iconAdd.setAttribute("class","fa fa-plus");
-    // let iconDel = document.createElement("i");
-    // iconDel.innerHTML = "delete";
-    // iconDel.setAttribute("class","material-icons");
-    // iconDel.addEventListener("click", deleteToDoList);
-    textdiv.appendChild(iconDel);
-    span.appendChild(textdiv);
-    span.appendChild(todoItem);
-    span.appendChild(iconAdd);
-    span.appendChild(dateAuthor);
-    div.appendChild(span);
-    document.getElementById("titlex").value="";
+        textdiv.setAttribute("class", "textDiv")
+        document.createElement("br");
+        // let todoItem = document.createElement("input");
+        // todoItem.setAttribute("class","taskInput");
+        // todoItem.type = 'text';
+        // let iconAdd = document.createElement("i");
+        // iconAdd.setAttribute("class","fa fa-plus");
+        // let iconDel = document.createElement("i");
+        // iconDel.innerHTML = "delete";
+        // iconDel.setAttribute("class","material-icons");
+        // iconDel.addEventListener("click", deleteToDoList);
+        textdiv.appendChild(iconDel);
+        span.appendChild(textdiv);
+        span.appendChild(todoItem);
+        span.appendChild(iconAdd);
+        span.appendChild(dateAuthor);
+        div.appendChild(span);
+        document.getElementById("titlex").value = "";
     }
-    iconAdd.addEventListener("click", function(){
+    iconAdd.addEventListener("click", function () {
         let todoItemValue = todoItem.value;
         let todoItemText = document.createTextNode(todoItemValue);
         let li = document.createElement("li");
         li.setAttribute("class", "list");
-        
+
         // let ul = document.createElement("ul");
         // ul.setAttribute("class", "uList");
         let close = document.createElement("i");
-        close.setAttribute("class","material-icons")
+        close.setAttribute("class", "material-icons")
         close.innerHTML = 'delete';
-        
-        if(todoItemValue === ''){
-           // alert("Please enter some value");
-        }
-        else{
+
+        if (todoItemValue === '') {
+            alert("Please enter some value");
+        } else {
             li.appendChild(todoItemText);
             ul.appendChild(li);
             spanParent.appendChild(ul)
             span.appendChild(spanParent);
         }
-        todoItem.value= '';
+        todoItem.value = '';
         li.appendChild(close);
         close.addEventListener("click", deleteToDoItem);
         li.addEventListener("click", strikeout);
-        
+
     });
 
 }
 
-function deleteToDoList(evt){
-    evt.target.parentNode.parentNode.style.display="none";
+function deleteToDoList(evt) {
+    evt.target.parentNode.parentNode.style.display = "none";
 }
 
-function strikeout(evt){
+function strikeout(evt) {
     evt.target.classList.toggle("strike");
 }
 
-function deleteToDoItem(evt){
+function deleteToDoItem(evt) {
     evt.target.parentNode.parentNode.removeChild(evt.target.parentNode);
 }
